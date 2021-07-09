a896461b2735626b76330e340b30b12b6009331f
Andy Zhao <17884966+Zhao-Andy@users.noreply.github.com>
2021-07-09 08:08:45 -0400

    Update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,7 +37,7 @@ module.exports = {
           title: 'Docs',
           items: [
             {
-              label: 'Tutorial',
+              label: 'Introduction',
               to: '/docs/intro',
             },
           ],
@@ -84,14 +84,14 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/forem/forem-docs/edit/master',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
-        },
+        // blog: {
+        //   showReadingTime: true,
+        //   // Please change this to your repo.
+        //   editUrl:
+        //     'https://github.com/forem/forem-docs/edit/master',
+        // },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
